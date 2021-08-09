29b3105b28c9779b0655efa47bedacff83763df5
tannerfunk <88557372+tannerfunk@users.noreply.github.com>
2021-08-09 12:06:29 -0600

    finished project @ meets expectations

--- js/script.js
+++ js/script.js
@@ -7,21 +7,53 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+//My object filled quotes array, where I store my quotes and the corresponding information.
+const quotes = [
+  { 
+    quote: "Believe with no evidence of how you will do it.",
+    source: "Brooke Castillo",
+    citation: "The Life Coach School"
+  },
+  { 
+    quote: "Without hard work, nothing grows but weeds.",
+    source: "Gordon B. Hinckley"
+  },
+  { 
+    quote: "My reading of history convinces me that most bad government results from too much government.",
+    source: "Thomas Jefferson"
+  },
+  {
+    quote: "Don't trade your authenticity for approval.",
+    source: "Kanye West"
+  },
+  { quote: "Some sincerely wish for more power in order to do good, but only a few individuals are good enough to be powerful.",
+    source: "Neal A. Maxwell",
+    year: "2000"
+  },
+];
 
+//The function to "pick out" a random quote.
+const getRandomQuote = () => {
+  let randomNumber = Math.floor( Math.random() * quotes.length);
+  let randomQuote = quotes[randomNumber];
+  return randomQuote;
+};
 
-
-/***
- * `getRandomQuote` function
-***/
-
-
-
-/***
- * `printQuote` function
-***/
+//The function to print that function nicely to the browser.
+const printQuote = () => {
+  let randomQuoteObject = getRandomQuote();
+  let html = `<p class="quote"> ${randomQuoteObject.quote} </p>
+  <p class="source"> ${randomQuoteObject.source}`;
+  if (randomQuoteObject.citation) {
+    html += `<span class="citation"> ${randomQuoteObject.citation} </span>`;
+  }
+  if (randomQuoteObject.year) {
+    html += `<span class="year"> ${randomQuoteObject.year} </span>`;
+  }
+  html += `</p>`;
+  document.getElementById('quote-box').innerHTML = html;
+  return html;
+};
 
 
 
